850a7b1af93cb25daad7f03f7eaff4e85d3baa2f
vuegoman <vuegoman@gmail.com>
2022-03-09 23:29:10 +0900

    added Docker file

--- src/templates/Logo.tsx
+++ src/templates/Logo.tsx
@@ -14,7 +14,12 @@ const Logo = (props: ILogoProps) => {
 
   return (
     <span className={`text-primary-100 inline-flex items-center ${fontStyle}`}>
-      <Image src="/favicon-sm.png" alt="logo" width={size} height={size} />
+      <Image
+        src="/logos/favicon-sm.png"
+        alt="logo"
+        width={size}
+        height={size}
+      />
       <path d="M0 0h24v24H0z" stroke="none" />
       <rect x="3" y="12" width="6" height="8" rx="1" />
       <rect x="9" y="8" width="6" height="12" rx="1" />
